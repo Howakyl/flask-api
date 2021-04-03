2e00c9d5a0c2213c6d5cee85a8b46f6e6de14254
Howakyl96 <howakyl@gmail.com>
2021-04-03 12:46:37 -0700

    database config

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, request
+# sql alchemy is an ORM used by flask to communicate with the DB
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
@@ -8,6 +9,22 @@ app = Flask(__name__)
 
 # Base Directory
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+# SQLite database
+# DATABASE = 'sqlite:///' + os.path.join(basedir, 'db.flask-reddit')
+
+# Local postgres database
+DATABASE = 'postgresql://localhost/flask-reddit'
+
+# setup database
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# init database
+db = SQLAlchemy(app)
+
+# init marshmallow
+marshmallow = Marshmallow(app)
 
 DEBUG = True
 PORT = 8000
