11ea57310af903bb3024561ddcd9732691808a24
Howakyl96 <howakyl@gmail.com>
2021-04-05 12:35:11 -0700

    created Post model

---
+++ Post.py
@@ -0,0 +1,30 @@
+# post model
+from app import db, marshmallow
+
+class Post(db.Model):
+    __table_args__ = {'extend_existing' : True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    timestap = db.Column(db.DateTime())
+    user = db.Column(db.String(100))
+    title = db.Column(db.String(200))
+    text = db.Column(db.String(500))
+    sub = db.Column(db.Integer, db.ForeignKey("sub.id"))
+
+    def __init__(self, user, title, text, sub):
+        self.user = user
+        self.title = title
+        self.text = text
+        self.sub = sub
+    
+    @classmethod
+    def create_post(cls, user, title, text, sub):
+        new_post = Post(user, title, text, sub)
+        try: 
+            db.session.add(new_post)
+            db.session.commit()
+        except:
+                db.session.rollback()
+                raise Exception('Session rollback')
+        return post_schema.jsonify(post)
+
